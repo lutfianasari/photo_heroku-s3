3dffeab7fbd4a2eaa6c77dea2beba66d382ee79f
lutfianasari <lutefnyew@gmail.com>
2013-05-06 18:29:40 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShowPhotoS3::Application.config.secret_token = '270a14ca07811daacbb86bac515a1423bcf6f26a3a9719b4e5bff0d4af619a7d2206b50c31ff1df8678cf1402db77e69888b3a5f7c94b9c56d29d217b02a1dce'
